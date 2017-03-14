62ac3d381ad5bb91bab5c73d133f08a87eba5b69
ironthrone <laozhuangg@gmail.com>
2017-03-14 18:28:18 +0800

    register listener

--- app/src/main/aidl/com/guo/rong/ILibrary.aidl
+++ app/src/main/aidl/com/guo/rong/ILibrary.aidl
@@ -2,9 +2,12 @@
 package com.guo.rong;
 
 import com.guo.artpractice.Book;
-// Declare any non-default types here with import statements
+import com.guo.rong.IOnNewBookArriveListener;
 
 interface ILibrary {
     List<Book> getBookList();
     Book searchBook(in String book);
+    void addBook(in Book book);
+    void registerOnNewBookArriveListener(in IOnNewBookArriveListener listener);
+    void unregisterOnNewBookArriveListener(in IOnNewBookArriveListener listener);
 }
--- app/src/main/java/com/guo/artpractice/ReaderActivity.java
+++ app/src/main/java/com/guo/artpractice/ReaderActivity.java
@@ -1,29 +1,37 @@
 package com.guo.artpractice;
 
+import android.app.ActivityManager;
 import android.app.Service;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.blankj.utilcode.utils.ProcessUtils;
 import com.guo.rong.ILibrary;
+import com.guo.rong.IOnNewBookArriveListener;
 
 import java.util.List;
 
 public class ReaderActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = ReaderActivity.class.getSimpleName();
+
     private ServiceConnection connection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             library = ILibrary.Stub.asInterface(service);
             try {
                 service.linkToDeath(deathRecipient, 0);
+                library.registerOnNewBookArriveListener(onNewBookArriveListener);
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
@@ -34,11 +42,34 @@ public class ReaderActivity extends AppCompatActivity implements View.OnClickLis
 //            bindService()
         }
     };
+
     IBinder.DeathRecipient deathRecipient = new IBinder.DeathRecipient() {
         @Override
         public void binderDied() {
             bindService(new Intent(ReaderActivity.this, LibraryService.class), connection, Service.BIND_AUTO_CREATE);
         }
+    };
+
+
+    private IOnNewBookArriveListener onNewBookArriveListener = new IOnNewBookArriveListener.Stub() {
+        @Override
+        public void onNewBookArrive(Book book) throws RemoteException {
+
+            Log.d(TAG, book.toString());
+
+            // 运行在主线程
+            Log.d(TAG, Thread.currentThread() == Looper.getMainLooper().getThread() ? "main" : "background");
+            // 根据Pid获取进程名称，运行在当前进程
+            int pid = android.os.Process.myPid();
+            ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+            List<ActivityManager.RunningAppProcessInfo> processInfos = activityManager.getRunningAppProcesses();
+            for (ActivityManager.RunningAppProcessInfo processInfo : processInfos) {
+                if (processInfo.pid == pid) {
+                    Log.d(TAG, processInfo.processName);
+                }
+            }
+        }
+
     };
 
     private ILibrary library;
@@ -52,12 +83,23 @@ public class ReaderActivity extends AppCompatActivity implements View.OnClickLis
         findViewById(R.id.search).setOnClickListener(this);
         findViewById(R.id.all)
                 .setOnClickListener(this);
+        findViewById(R.id.add)
+                .setOnClickListener(this);
+        findViewById(R.id.unregister)
+                .setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.unregister:
+                try {
+                    library.unregisterOnNewBookArriveListener(onNewBookArriveListener);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+                break;
             case R.id.search:
                 try {
                     Book book = library.searchBook("Android");
@@ -76,6 +118,13 @@ public class ReaderActivity extends AppCompatActivity implements View.OnClickLis
                 try {
                     List<Book> books = library.getBookList();
                     Log.d(TAG, books.toString());
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.add:
+                try {
+                    library.addBook(new Book("Stock","Guo",44));
                 } catch (RemoteException e) {
                     e.printStackTrace();
                 }
