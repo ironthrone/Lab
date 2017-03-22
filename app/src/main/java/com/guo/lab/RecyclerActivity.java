7addfbc0967d94f2d6ec67c55756b3962416037f
ironthrone <laozhuangg@gmail.com>
2017-03-22 15:57:44 +0800

    recycler load more

--- app/src/main/java/com/guo/lab/RecyclerActivity.java
+++ app/src/main/java/com/guo/lab/RecyclerActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.blankj.utilcode.utils.ToastUtils;
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -13,21 +14,23 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import rx.Observable;
+import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
 
-public class RecyclerActivity extends AppCompatActivity implements BaseQuickAdapter.RequestLoadMoreListener {
+public class RecyclerActivity extends AppCompatActivity implements BaseQuickAdapter.RequestLoadMoreListener, BaseQuickAdapter.OnItemClickListener {
 
-    private String[] heros = {"武松", "林冲", "鲁达", "张清"};
+    private String[] heros = {"武松", "林冲", "鲁达", "张清", "武松", "林冲", "鲁达", "张清", "武松", "林冲", "鲁达", "张清", "武松", "林冲", "鲁达", "张清", "武松", "林冲", "鲁达", "张清"};
     private List<String> heroList;
 
     {
         heroList = Arrays.asList(heros);
     }
 
-    BaseQuickAdapter<String,BaseViewHolder> mAdapter = new BaseQuickAdapter<String, BaseViewHolder>(android.R.layout.simple_list_item_1, heroList) {
+    BaseQuickAdapter<String, BaseViewHolder> mAdapter = new BaseQuickAdapter<String, BaseViewHolder>(android.R.layout.simple_list_item_1, heroList) {
         @Override
         protected void convert(BaseViewHolder helper, String item) {
             helper.setText(android.R.id.text1, item);
@@ -40,8 +43,8 @@ public class RecyclerActivity extends AppCompatActivity implements BaseQuickAdap
         setContentView(R.layout.activity_recycler);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
 
-        mAdapter.setEnableLoadMore(true);
-        mAdapter.setOnLoadMoreListener(this,recyclerView);
+        mAdapter.setOnLoadMoreListener(this, recyclerView);
+        mAdapter.setOnItemClickListener(this);
         recyclerView.setAdapter(mAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
     }
@@ -49,15 +52,47 @@ public class RecyclerActivity extends AppCompatActivity implements BaseQuickAdap
 
     @Override
     public void onLoadMoreRequested() {
-        ToastUtils.showLongToastSafe("Load more");
-        rx.Observable.timer(3000, TimeUnit.MILLISECONDS)
-                .observeOn(Schedulers.io())
-                .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<Long>() {
+
+        Observable.create(new Observable.OnSubscribe<Object>() {
+            @Override
+            public void call(Subscriber<? super Object> subscriber) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                subscriber.onCompleted();
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Object>() {
                     @Override
-                    public void call(Long aLong) {
-                        mAdapter.loadMoreComplete();
+                    public void onCompleted() {
+
+                        //loadMoreComplete()不会隐藏loadmore view,true的意思是隐藏
+                        mAdapter.loadMoreEnd(true);
+                        ToastUtils.showShortToastSafe("Load more");
+
                     }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+
+                    }
+
                 });
+
+
+    }
+
+    @Override
+    public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+        ToastUtils.showShortToastSafe(adapter.getItem(position).toString());
     }
 }
