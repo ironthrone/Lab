b841075b1be73c9ecf850ae75b6ec9e771119926
ironthrone <laozhuangg@gmail.com>
2017-04-11 15:23:28 +0800

    add wake lock

--- app/src/main/java/com/guo/lab/ForegroundService.java
+++ app/src/main/java/com/guo/lab/ForegroundService.java
@@ -2,13 +2,16 @@ package com.guo.lab;
 
 import android.app.Notification;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.blankj.utilcode.utils.LogUtils;
 
@@ -18,6 +21,7 @@ public class ForegroundService extends Service {
 
     private CirculationLogger logger;
     private KeepLiveReceiver keepLiveReceiver;
+    private PowerManager.WakeLock wakeLock;
 
     public ForegroundService() {
     }
@@ -31,6 +35,11 @@ public class ForegroundService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "weak");
+        wakeLock.acquire();
+
         LogUtils.d(new Date() + " onCreate");
         logger = new CirculationLogger("normalservice.txt");
         logger.circulationLog("circulation");
@@ -74,6 +83,7 @@ public class ForegroundService extends Service {
     public void onDestroy() {
         super.onDestroy();
         stopForeground(true);
+        if(wakeLock.isHeld()) wakeLock.release();
         unregisterReceiver(keepLiveReceiver);
         logger.insertLog("on destroy");
     }
