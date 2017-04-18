4b28178cf6ae9dba8f3cd380b063ba2527e98bdb
ironthrone <laozhuangg@gmail.com>
2017-04-18 17:01:19 +0800

    select image from gallary

--- app/src/main/java/com/guo/lab/MainActivity.java
+++ app/src/main/java/com/guo/lab/MainActivity.java
@@ -1,7 +1,9 @@
 package com.guo.lab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity  {
@@ -12,6 +14,13 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.select_image)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(MainActivity.this, SelectImageActivity.class));
+                    }
+                });
 
     }
 }
